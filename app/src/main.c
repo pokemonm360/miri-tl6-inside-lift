7006118a2989affa3a96d79e8128a3bd7af335e7
Kostas Čaplinskas <kostascaplinskas@Kostass-MacBook-Air-5.local>
2026-02-15 23:08:47 +0200

    Polling blue button

--- app/src/main.c
+++ app/src/main.c
@@ -1,80 +1,42 @@
-/*
- * Copyright (c) 2021 Nordic Semiconductor ASA
- * SPDX-License-Identifier: Apache-2.0
- */
-
+#include <stdio.h>
 #include <zephyr/kernel.h>
-#include <zephyr/drivers/sensor.h>
-#include <zephyr/logging/log.h>
+#include <zephyr/drivers/gpio.h>
 
-#include <app/drivers/blink.h>
+//LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
 
-#include <app_version.h>
-
-LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
-
-#define BLINK_PERIOD_MS_STEP 100U
-#define BLINK_PERIOD_MS_MAX  1000U
+static const int32_t sleep_time_ms = 1000;
+static const struct gpio_dt_spec btn = GPIO_DT_SPEC_GET(DT_ALIAS(button_blue), gpios);
 
 int main(void)
 {
-	int ret;
-	unsigned int period_ms = BLINK_PERIOD_MS_MAX;
-	const struct device *sensor, *blink;
-	struct sensor_value last_val = { 0 }, val;
+    int ret;
+    int state;
 
-	printk("Zephyr Example Application %s\n", APP_VERSION_STRING);
+    if(!gpio_is_ready_dt(&btn)) {
+        printk("ERROR: button not ready\n");
+        return 0;
+    }
 
-	sensor = DEVICE_DT_GET(DT_NODELABEL(example_sensor));
-	if (!device_is_ready(sensor)) {
-		LOG_ERR("Sensor not ready");
-		return 0;
-	}
+    ret = gpio_pin_configure_dt(&btn, GPIO_INPUT);
+    if(ret < 0) {
+        printk("ERROR: failed to configure button pin\n");
+        return 0;
+    }
 
-	blink = DEVICE_DT_GET(DT_NODELABEL(blink_led));
-	if (!device_is_ready(blink)) {
-		LOG_ERR("Blink LED not ready");
-		return 0;
-	}
+    printk("Button spec flags: 0x%x\n", btn.dt_flags);
 
-	ret = blink_off(blink);
-	if (ret < 0) {
-		LOG_ERR("Could not turn off LED (%d)", ret);
-		return 0;
-	}
+    while(1){
+        state = gpio_pin_get_dt(&btn);
+        if(state < 0) {
+            printk("ERROR %d: failed to read button state\n", state);
+        
+        }
+        else {
+            printk("INFO: button state is %d\n", state);
+        }
 
-	printk("Use the sensor to change LED blinking period\n");
+        k_msleep(sleep_time_ms);
+    }
 
-	while (1) {
-		ret = sensor_sample_fetch(sensor);
-		if (ret < 0) {
-			LOG_ERR("Could not fetch sample (%d)", ret);
-			return 0;
-		}
-
-		ret = sensor_channel_get(sensor, SENSOR_CHAN_PROX, &val);
-		if (ret < 0) {
-			LOG_ERR("Could not get sample (%d)", ret);
-			return 0;
-		}
-
-		if ((last_val.val1 == 0) && (val.val1 == 1)) {
-			if (period_ms == 0U) {
-				period_ms = BLINK_PERIOD_MS_MAX;
-			} else {
-				period_ms -= BLINK_PERIOD_MS_STEP;
-			}
-
-			printk("Proximity detected, setting LED period to %u ms\n",
-			       period_ms);
-			blink_set_period_ms(blink, period_ms);
-		}
-
-		last_val = val;
-
-		k_sleep(K_MSEC(100));
-	}
-
-	return 0;
+    return 0;
 }
-
